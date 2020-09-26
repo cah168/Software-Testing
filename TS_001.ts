0b4c4e5286b9cc4ff17622b81f4b024641377802
cah168 <59241082+cah168@users.noreply.github.com>
2020-09-26 16:49:32 +0700

    c
    c

---
+++ TS_001.ts
@@ -0,0 +1,50 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_001</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>a272815d-f742-442e-9ace-d88c09ab6868</testSuiteGuid>
+   <testCaseLink>
+      <guid>0bd1de70-80ad-410b-81c2-1c2f82c996ae</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_001</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>60e0541c-17b5-43a9-acf3-f33ff62bbdfb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_002</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5b3f4dc0-7818-4d01-ba40-aa43997671c3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_003</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9c50f6f4-7b00-44b2-b089-e5bd0213e9a7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_004</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1fe945b6-e670-4a34-ac62-08ae00c06372</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_005</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>fa695244-9fe1-4077-ad8e-16888822f5d6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_006</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
